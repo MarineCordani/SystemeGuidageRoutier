d95f17dd698b58599778d8f96f0591c45fd4609c
Alexandre <alex.m.petrin@hotmail.com>
2018-03-31 10:01:34 -0400

    Creation de la boucle d'execution dans le moteur de traitement. Pas encore complete

--- src/Controlleur/MoteurTraitement.java
+++ src/Controlleur/MoteurTraitement.java
@@ -5,7 +5,7 @@ import javax.swing.SwingUtilities;
 
 import Modele.Intersection;
 import Modele.ReseauRoutier;
-import Modele.Trajet;
+import Modele.Vehicule;
 import Vue.EcranGPS;
 
 public class MoteurTraitement {
@@ -24,6 +24,36 @@ public class MoteurTraitement {
 
 		Trajet trajet = GenerateurTrajet.genererTrajet(a, b);
 		trajet.imprimerTrajet();*/
+		
+		
+		//Créer boucle d'execution
+		GenerateurVehicule gv = new GenerateurVehicule();
+		Vehicule vehiculeTemporaire = null;
+		Intersection intersectionTemporaire = null;
+		
+		GenerateurAccident ga = new GenerateurAccident();
+		
+		do{
+			//Generer voiture
+			vehiculeTemporaire = gv.genererVehicule();
+			intersectionTemporaire = null;//TODO ERREUR, doit obtenir intersection de depart du vehiculeTemporaire
+			//getReseauRoutier().ajouterVehicule(vehiculeTemporaire, intersectionTemporaire);
+			
+			//Generer accident
+			ga.genererAccident();
+			
+			//Faire avancer vehicule
+			//TODO: Faire deplacer les vehicules
+			
+			//Attente de quelques milisecondes pour que l'animation soit visible
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				
+			}
+
+		} while (true);
+		
 	}
 
 	/**

3b4b5771959036b2478bcc3b6045c6dd96ae15be
Vestine Mukeshimana <mukv01@uqo.ca>
2018-04-11 21:23:13 -0400

    On a empêché une multitude de message de congestion

--- src/Controleur/VerificateurCongestion.java
+++ src/Controleur/VerificateurCongestion.java
@@ -4,12 +4,15 @@ import java.util.Vector;
 
 import Modele.Arthere;
 
-public class VerificateurCongestion {		
+public class VerificateurCongestion {	
+	private boolean siChangementEtat = false;
 	public void verifierCongestion(){
 			
 		Vector<Arthere> artheres = MoteurTraitement.getReseauRoutier().getArtheres();
 		for(Arthere a: artheres) {
-			if(a.verifierCongestion()) {
+			boolean presenceCongestion = a.getPresenceCongestion();
+			boolean congestionDetecte = a.verifierCongestion();
+			if(presenceCongestion == false && congestionDetecte) {
 				MoteurTraitement.getEcran().ajouterTexteAuJournal("L'arthère " + a.getIdentifiant() + " est congestionnée");
 			}
 		}
